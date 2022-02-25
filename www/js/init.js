6ab3c40f336b34f9bab2cb764c835e51ec561a65
A80167811050629\alum-01 <adriacostasalse@gmail.com>
2022-02-25 17:03:34 +0100

    first commit

---
+++ www/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    //$('.parallax').parallax();
+    $('.tabs').tabs({"swipeable":true});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
